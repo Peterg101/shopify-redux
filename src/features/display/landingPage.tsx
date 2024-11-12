e5915de018ed88488fdcf00c247f0355a294c00d
Peterg101 <pete.goon@googlemail.com>
2024-11-12 10:05:03 +0000

    button added

--- src/features/display/landingPage.tsx
+++ src/features/display/landingPage.tsx
@@ -1,14 +1,20 @@
-import { Box } from "@mui/material"
+import { Box, Button } from "@mui/material"
 import UserInterface from "../userInterface/userInterface"
 import { MainOptions } from "./mainOptions"
 import { useState } from "react";
 
 export const LandingPage = () => {
     const [actualFile, setActualFile] = useState<File | null>(null);
+
+    const handleLogin = () => {
+        // Redirect to FastAPI Google login endpoint
+        window.location.href = "http://google.co.uk";
+      };
     return(
         <Box sx = {{marginTop: 10}}>
             <UserInterface/>
             <MainOptions/>
+            <Button onClick={handleLogin}>Call Google</Button>
         </Box>
         
     )
