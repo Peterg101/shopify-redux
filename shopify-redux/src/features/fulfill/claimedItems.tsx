dcac9bf9e172b321c51c53c140f6d5fac6b78a5d
Peterg101 <pete.goon@googlemail.com>
2026-01-15 12:42:33 +0000

    styles updated

--- shopify-redux/src/features/fulfill/claimedItems.tsx
+++ shopify-redux/src/features/fulfill/claimedItems.tsx
@@ -1,12 +1,37 @@
-import { Box } from "@mui/material"
+import { Box, useTheme, Typography, Stack, Container } from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
 
 export const ClaimedItems = () => {
-    return(
-        <Box>
-            <h1>Claimed Items</h1>
-            <h1>Claimed Items</h1>
-            <h1>Claimed Items</h1>
-            <h1>Claimed Items</h1>
-        </Box>
-    )
-}
+  const theme = useTheme();
+  const userInterfaceState = useSelector(
+    (state: RootState) => state.userInterfaceState
+  );
+
+  const drawerWidth = 100;
+
+  return (
+    <Box
+      sx={{
+        mt: 10,
+        ...(userInterfaceState.leftDrawerOpen && {
+          ml: `${drawerWidth}px`,
+          width: `calc(100% - ${drawerWidth}px)`,
+          transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+        }),
+      }}
+    >
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Stack spacing={2}>
+          <Typography variant="h5">Claimed Items</Typography>
+          <Typography variant="h5">Claimed Items</Typography>
+          <Typography variant="h5">Claimed Items</Typography>
+          <Typography variant="h5">Claimed Items</Typography>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
--- shopify-redux/src/features/fulfill/fulfilledOrClaimed.tsx
+++ shopify-redux/src/features/fulfill/fulfilledOrClaimed.tsx
@@ -1,17 +1,49 @@
-import { Box } from "@mui/material"
+import { Box, Tabs, Tab, useTheme } from "@mui/material"
 import { useState } from "react";
 import { FulfillableItems } from "./fulfillableItems";
 import { ClaimedItems } from "./claimedItems";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 export const FulfillOrClaimed = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const theme = useTheme();
 
-    const [fulfillMode, setFulfillMode] = useState(true)
-    return(
-        <Box>
-            {(fulfillMode) ?
-                        <FulfillableItems/>  :
-                        <ClaimedItems/>
-                        }
-        </Box>
-    )
-}
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setTabIndex(newValue);
+  };
+
+  return (
+    <Box>
+      <Tabs
+        value={tabIndex}
+        onChange={handleChange}
+        centered
+        sx={{
+          position: "sticky",
+          top: theme.mixins.toolbar.minHeight,
+          backgroundColor: "background.paper",
+          zIndex: theme.zIndex.appBar - 1,
+          borderBottom: 1,
+          borderColor: "divider",
+        }}
+      >
+        <Tab
+          icon={<AssignmentIcon />}
+          iconPosition="start"
+          label="Available to Fulfill"
+        />
+        <Tab
+          icon={<CheckCircleIcon />}
+          iconPosition="start"
+          label="My Claimed Items"
+        />
+      </Tabs>
+
+      <Box>
+        {tabIndex === 0 && <FulfillableItems />}
+        {tabIndex === 1 && <ClaimedItems />}
+      </Box>
+    </Box>
+  );
+};
